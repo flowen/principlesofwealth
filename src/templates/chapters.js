6cc6804ce0e79ff3471fed2c30f8d1f4b94337b0
fLowen <lowen@flowen.nl>
2019-08-31 20:02:37 +0200

    conditional chapters

--- src/templates/chapters.js
+++ src/templates/chapters.js
@@ -33,22 +33,26 @@ const ChaptersPage = ({ pageContext }) => {
 
       <h2 className="subtitle">A summary of Naval Ravikant’s tweetstorms and podcast</h2>
 
-      <nav className="chapters">
-        <ol className="chapters__list">
-          {chapters.map(({ node }) => {
-            const { id, frontmatter } = node
-            const { title, path } = frontmatter
+      {chapters.length > 1 ? (
+        <nav className="chapters">
+          <ol className="chapters__list">
+            {chapters.map(({ node }) => {
+              const { id, frontmatter } = node
+              const { title, path } = frontmatter
 
-            return (
-              <li className="chapters__item" key={id}>
-                <Link to={path} className="chapters__anchor">
-                  {title}
-                </Link>
-              </li>
-            )
-          })}
-        </ol>
-      </nav>
+              return (
+                <li className="chapters__item" key={id}>
+                  <Link to={path} className="chapters__anchor">
+                    {title}
+                  </Link>
+                </li>
+              )
+            })}
+          </ol>
+        </nav>
+      ) : (
+        <div>Got nuthin</div>
+      )}
     </Layout>
   )
 }
