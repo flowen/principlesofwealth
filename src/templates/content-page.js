ef6a197c341b67fdf7859921ea97ba9ea06a674d
fLowen <lowen@flowen.nl>
2019-09-05 08:58:22 +0200

    updated content page, grids and multi-column layout

--- src/templates/content-page.js
+++ src/templates/content-page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Nav from '../components/nav'
 
 const TemplateContent = ({ data }) => {
   const { markdownRemark } = data
@@ -18,12 +19,15 @@ const TemplateContent = ({ data }) => {
           <br /> .net
         </Link>
 
+        <Nav />
+
         <h1 className="title">{frontmatter.title}</h1>
 
         <h2 className="subtitle">{frontmatter.subtitle}</h2>
       </header>
 
       <div className="content-page__article-wrapper">
+        {frontmatter.description ? <p className="intro">{frontmatter.description}</p> : ''}
         <article className="content-page__article" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
